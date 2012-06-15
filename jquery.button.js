33b6fe04009582f84c00527365fc98056e43a29a
zzzzBov <zzzzBov@gmail.com>
2012-06-15 17:03:26 -0400

    Massive updates to jquery.button.js
    added basic level of state handling
    **does not yet manage enabled/disabled states**
    
    reordered event triggers and used hex codes
    abstracted mouse and key trigger management

--- jquery.button.js
+++ jquery.button.js
@@ -6,7 +6,9 @@
  */
 (function ($, undefined) {
     "use strict";
-    var widget;
+    var widget,
+        mouseTriggers,
+        keyTriggers;
     widget = 'button';
 //------------------------------------------------------------------------------
     //widget constructor
@@ -20,6 +22,61 @@
         this.options($.extend({}, $[widget].prototype._options, options));
     };
 //------------------------------------------------------------------------------
+    //binary values for specific button press triggers
+    $[widget].triggers = {
+        none:               0x00000000,
+        click:              0x00000001,
+        doubleclick:        0x00000002,
+        mousedown:          0x00000004,
+        mousehold:          0x00000008,
+        mouseup:            0x00000010,
+        leftclick:          0x00000020,
+        rightclick:         0x00000040,
+        middleclick:        0x00000080,
+        leftdoubleclick:    0x00000100,
+        rightdoubleclick:   0x00000200,
+        middledoubleclick:  0x00000400,
+        leftmousedown:      0x00000800,
+        rightmousedown:     0x00001000,
+        middlemousedown:    0x00002000,
+        leftmousehold:      0x00004000,
+        rightmousehold:     0x00008000,
+        middlemousehold:    0x00010000,
+        leftmouseup:        0x00020000,
+        rightmouseup:       0x00040000,
+        middlemouseup:      0x00080000,
+        keydown:            0x00100000,
+        keypress:           0x00200000,
+        keyup:              0x00400000
+    };
+//------------------------------------------------------------------------------
+    mouseTriggers = {
+        all: $.button.triggers.click |
+            $.button.triggers.doubleclick |
+            $.button.triggers.mousedown |
+            $.button.triggers.mousehold |
+            $.button.triggers.mouseup,
+        '1': $.button.triggers.leftclick |
+            $.button.triggers.leftdoubleclick |
+            $.button.triggers.leftmousedown |
+            $.button.triggers.leftmousehold |
+            $.button.triggers.leftmouseup,
+        '2': $.button.triggers.middleclick |
+            $.button.triggers.middledoubleclick |
+            $.button.triggers.middlemousedown |
+            $.button.triggers.middlemousehold |
+            $.button.triggers.middlemouseup,
+        '3': $.button.triggers.rightclick |
+            $.button.triggers.rightdoubleclick |
+            $.button.triggers.rightmousedown |
+            $.button.triggers.rightmousehold |
+            $.button.triggers.rightmouseup
+    };
+    keyTriggers =
+        $.button.triggers.keydown |
+        $.button.triggers.keypress |
+        $.button.triggers.keyup;
+//------------------------------------------------------------------------------
     $[widget].prototype = {
         //the widget's state classes
         _classes: {
@@ -28,20 +85,29 @@
             focused:    'is-focused',
             blurred:    'is-blurred',
             over:       'is-over',
-            out:        'is-out'
+            out:        'is-out',
+            up:         'is-up',
+            down:       'is-down'
         },
         //the widget's default options
         _options: {
             disabled: false,
-            tabindex: true
+            keys: [13, 32],
+            tabindex: true,
+            triggers: 0x00200001 //click and keypress
         },
+        _mousepressed: false,
+        _mouseover: false,
+        _keypressed: false,
         //accessor for _options
         _getOption: function (key) {
             return this._options[key];
         },
         //mutator for _options
         _setOption: function (key, value) {
-            var options;
+            var options,
+                self;
+            self = this;
             options = this._options;
             switch (key) {
             case 'disabled':
@@ -63,6 +129,15 @@
                     value = -1;
                 }
                 this._element.attr('tabindex', options.disabled ? -1 : value);
+                break;
+            case 'keys':
+                this._keys = {};
+                if (!$.isArray(value)) {
+                    value = (''+value).split(' ');
+                }
+                $.each(value, function (i, key) {
+                    self._keys[key] = true;
+                });
                 break;
             default:
                 //by default, do nothing
@@ -90,20 +165,24 @@
                 'tabindex': 0
             }).on({
                 'click.button':         $.proxy(this, '_clickHandler'),
-                'dblclick.button':      $.proxy(this, '_dblClickHandler'),
-                'mousedown.button':     $.proxy(this, '_mouseDownHandler'),
-                'mouseup.button':       $.proxy(this, '_mouseUpHandler'),
+                'dblclick.button':      $.proxy(this, '_dblclickHandler'),
+                'mousedown.button':     $.proxy(this, '_mousedownHandler'),
+                'mouseup.button':       $.proxy(this, '_mouseupHandler'),
                 'focus.button':         $.proxy(this, '_focusHandler'),
                 'blur.button':          $.proxy(this, '_blurHandler'),
                 'mouseenter.button':    $.proxy(this, '_mouseenterHandler'),
                 'mouseleave.button':    $.proxy(this, '_mouseleaveHandler'),
-                'keydown.button':       $.proxy(this, '_keyDownHandler'),
-                'keyup.button':         $.proxy(this, '_keyUpHandler'),
-                'keypress.button':      $.proxy(this, '_keyPressHandler')
+                'keydown.button':       $.proxy(this, '_keydownHandler'),
+                'keyup.button':         $.proxy(this, '_keyupHandler'),
+                'keypress.button':      $.proxy(this, '_keypressHandler')
             }).addClass(c.out)
+                .addClass(c.up)
                 .toggleClass(c.focused, focused)
                 .toggleClass(c.blurred, !focused);
-            $(document).on('mouseup.button', $.proxy(this, '_documentMouseUpHandler'));
+            $(document).on({
+                'mouseup.button':   $.proxy(this, '_documentMouseupHandler'),
+                'keyup.button':     $.proxy(this, '_documentKeyupHandler')
+            });
         },
         //_init called as the constructor,
         //or any time the widget is called with no arguments
@@ -144,9 +223,16 @@
             }
             element.removeClass(classList.join(' '));
             
+            //end timeouts and intervals
+            clearTimeout(this._holdTimeout);
+            clearInterval(this._holdInterval);
+            
             //unbind events
             element.off('.button');
-            $(document).off('mouseup.button', this._documentMouseUpHandler);
+            $(document).off({
+                'mouseup.button':   this._documentMouseupHandler,
+                'keyup.button':     this._documentKeyupHandler
+            });
             //remove the widget reference
             delete element.data()[widget];
         },
@@ -198,17 +284,97 @@
         widget: function () {
             return this._element;
         },
+        startpress: function () {
+            var element,
+                classes;
+            element = this._element;
+            classes = this._classes;
+            element.addClass(classes.down).removeClass(classes.up);
+        },
+        press: function () {
+            this._element.trigger('buttonpress');
+        },
+        endpress: function () {
+            var element,
+                classes;
+            element = this._element;
+            classes = this._classes;
+            element.addClass(classes.up).removeClass(classes.down);
+        },
+        _mouseTriggersEvent: function (mousebutton, event) {
+            var triggers;
+            triggers = this._options.triggers;
+            switch (arguments.length) {
+            case 1:
+                if ((triggers & mouseTriggers.all) ||
+                    (triggers & mouseTriggers[mousebutton])) {
+                    return true;
+                }
+                return false;
+            case 2:
+            default:
+                if ((triggers & $.button.triggers[event]) ||
+                    (mousebutton === 1 && (triggers & $.button.triggers['left' + event])) ||
+                    (mousebutton === 2 && (triggers & $.button.triggers['middle' + event])) ||
+                    (mousebutton === 3 && (triggers & $.button.triggers['right' + event]))) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        _keyTriggersEvent: function (key, event) {
+            var triggers,
+                keys;
+            triggers = this._options.triggers;
+            keys = this._keys;
+            switch (arguments.length) {
+            case 1:
+                //check if key is in keys
+                //check if triggers is one of <key event types>
+                if (keys[key] && (triggers & keyTriggers)) {
+                    return true;
+                }
+                return false;
+            case 2:
+            default:
+                //check if key is in keys
+                //check if triggers is $.button.triggers[event]
+                if (keys[key] && (triggers & $.button.triggers[event])) {
+                    return true;
+                }
+                return false;
+            }
+        },
         _clickHandler: function (e) {
-            
+            if (this._mouseTriggersEvent(e.which, 'click')) {
+                this.press();
+            }
         },
-        _dblClickHandler: function (e) {
-            
+        _dblclickHandler: function (e) {
+            if (this._mouseTriggersEvent(e.which, 'doubleclick')) {
+                this.press();
+            }
         },
-        _mouseDownHandler: function (e) {
-            
+        _mousedownHandler: function (e) {
+            if (this._mouseTriggersEvent(e.which)) {
+                this.startpress();
+                this._mousepressed = true;
+            }
+            if (this._mouseTriggersEvent(e.which, 'mousedown')) {
+                this.press();
+            }
+            if (this._mouseTriggersEvent(e.which, 'mousehold')) {
+                this.press();
+                this._holdTimeout = setTimeout($.proxy(this, '_holdTimeoutCallback'), 200);
+                //start timeout for 200ms
+                //timeout will start interval which will fire every 50ms
+                //triggering press
+            }
         },
-        _mouseUpHandler: function (e) {
-            
+        _mouseupHandler: function (e) {
+            if (this._mouseTriggersEvent(e.which, 'mouseup')) {
+                this.press();
+            }
         },
         _focusHandler: function (e) {
             var classes;
@@ -224,20 +390,60 @@
             var classes;
             classes = this._classes;
             this._element.addClass(classes.over).removeClass(classes.out);
+            this._mouseover = true;
+            if (this._mousepressed) {
+                this.startpress();
+            }
         },
         _mouseleaveHandler: function (e) {
             var classes;
             classes = this._classes;
             this._element.addClass(classes.out).removeClass(classes.over);
+            this._mouseover = false;
+            if (this._mousepressed) {
+                this.endpress();
+            }
         },
-        _keyDownHandler: function (e) {
-            
+        _keydownHandler: function (e) {
+            if (this._keyTriggersEvent(e.which)) {
+                this.startpress();
+                this._keypressed = true;
+            }
+            if (this._keyTriggersEvent(e.which, 'keydown')) {
+                this.press();
+            }
         },
-        _keyUpHandler: function (e) {
-            
+        _keyupHandler: function (e) {
+            if (this._keyTriggersEvent(e.which, 'keyup')) {
+                this.press();
+            }
         },
-        _keyPressHandler: function (e) {
-            
+        _keypressHandler: function (e) {
+            if (this._keyTriggersEvent(e.which, 'keypress')) {
+                this.press();
+            }
+        },
+        _documentMouseupHandler: function (e) {
+            if (this._mouseTriggersEvent(e.which)) {
+                this.endpress();
+                this._mousepressed = false;
+            }
+            clearTimeout(this._holdTimeout);
+            clearInterval(this._holdInterval);
+        },
+        _documentKeyupHandler: function (e) {
+            if (this._keyTriggersEvent(e.which)) {
+                this.endpress();
+                this._keypressed = false;
+            }
+        },
+        _holdTimeoutCallback: function (e) {
+            this._holdInterval = setInterval($.proxy(this, '_holdIntervalCallback'), 50);
+        },
+        _holdIntervalCallback: function (e) {
+            if (this._mouseover) {
+                this.press();
+            }
         }
     };
 //------------------------------------------------------------------------------
